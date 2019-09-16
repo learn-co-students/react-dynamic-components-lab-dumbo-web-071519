b2c7631bbb45144372f02d75298af7b94a2dfacc
github username <github email address>
2019-09-16 14:30:35 -0400

    Components Within Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import BlogPost from './BlogPost.js'
+import Comment from './Comment.js'
 import ColorBox from './ColorBox.js'
+
 
 class App extends Component {
   
